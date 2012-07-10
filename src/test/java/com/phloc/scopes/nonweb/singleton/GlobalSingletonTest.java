804a0e9bb7df5a511471c016e4e88b0bd90e021d
ph <ph@phloc.com>
2012-07-10 12:29:55 +0000

    Improved tests

--- src/test/java/com/phloc/scopes/nonweb/singleton/GlobalSingletonTest.java
+++ src/test/java/com/phloc/scopes/nonweb/singleton/GlobalSingletonTest.java
@@ -21,15 +21,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 
-import javax.annotation.Nonnull;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.phloc.commons.annotations.UsedViaReflection;
 import com.phloc.scopes.nonweb.mock.AbstractScopeAwareTestCase;
-import com.phloc.scopes.nonweb.singleton.GlobalSingleton;
 
 /**
  * Test class for class {@link GlobalSingleton}.
@@ -38,48 +35,18 @@ import com.phloc.scopes.nonweb.singleton.GlobalSingleton;
  */
 public final class GlobalSingletonTest extends AbstractScopeAwareTestCase
 {
-  private static int s_nCtorCount = 0;
-  private static int s_nDtorCount = 0;
-
-  /**
-   * Default use case example.
-   * 
-   * @author philip
-   */
-  public static final class MockGlobalSingleton extends GlobalSingleton
-  {
-    @Deprecated
-    @UsedViaReflection
-    public MockGlobalSingleton ()
-    {
-      s_nCtorCount++;
-    }
-
-    @Nonnull
-    public static MockGlobalSingleton getInstance ()
-    {
-      return getGlobalSingleton (MockGlobalSingleton.class);
-    }
-
-    @Override
-    protected void onDestroy () throws Exception
-    {
-      s_nDtorCount++;
-    }
-  }
-
   @BeforeClass
   public static void beforeClass ()
   {
-    assertEquals (0, s_nCtorCount);
-    assertEquals (0, s_nDtorCount);
+    assertEquals (0, MockGlobalSingleton.s_nCtorCount);
+    assertEquals (0, MockGlobalSingleton.s_nDtorCount);
   }
 
   @AfterClass
   public static void afterClass ()
   {
-    assertEquals (1, s_nCtorCount);
-    assertEquals (1, s_nDtorCount);
+    assertEquals (1, MockGlobalSingleton.s_nCtorCount);
+    assertEquals (1, MockGlobalSingleton.s_nDtorCount);
   }
 
   @Test
