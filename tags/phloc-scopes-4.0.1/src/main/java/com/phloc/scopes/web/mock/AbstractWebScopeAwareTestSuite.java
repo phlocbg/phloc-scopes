26c2ded77bade09268a95a1304bb43397772bb42
ph@phloc.com <ph@phloc.com@1e497704-d800-dfe1-6598-1dc77ae1aeb0>
2012-07-23 18:09:59 +0000

    [maven-release-plugin]  copy for tag phloc-scopes-4.0.1

---
+++ tags/phloc-scopes-4.0.1/src/main/java/com/phloc/scopes/web/mock/AbstractWebScopeAwareTestSuite.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.scopes.web.mock;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+import javax.servlet.http.HttpSession;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import com.phloc.commons.mock.AbstractPhlocTestCase;
+
+/**
+ * Base class where the initialization of the scopes happens once before the
+ * class and once afterwards. This is faster than performing the initialization
+ * before each test separately.
+ * 
+ * @author philip
+ */
+@Immutable
+public abstract class AbstractWebScopeAwareTestSuite extends AbstractPhlocTestCase
+{
+  @BeforeClass
+  public static void beforeClass () throws Exception
+  {
+    WebScopeAwareTestSetup.setupScopeTests ();
+  }
+
+  @Nonnull
+  protected final MockServletContext getServletContext ()
+  {
+    return WebScopeAwareTestSetup.getServletContext ();
+  }
+
+  @Nonnull
+  protected final MockHttpServletRequest getRequest ()
+  {
+    return WebScopeAwareTestSetup.getRequest ();
+  }
+
+  @Nonnull
+  protected final HttpSession getSession (final boolean bCreateIfNotExisting)
+  {
+    return WebScopeAwareTestSetup.getSession (bCreateIfNotExisting);
+  }
+
+  @AfterClass
+  public static void afterClass () throws Exception
+  {
+    WebScopeAwareTestSetup.shutdownScopeTests ();
+  }
+}
---
+++ tags/phloc-scopes-4.0.1/src/main/java/com/phloc/scopes/web/util/SessionBackedRequestFieldData.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.scopes.web.util;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.commons.string.StringHelper;
+import com.phloc.scopes.IScope;
+import com.phloc.scopes.web.mgr.WebScopeManager;
+
+/**
+ * A specialized request field that uses a value stored in the session as the
+ * default, in case no parameter is passed. If no value is in the session either
+ * the hard coded default value is used.
+ * 
+ * @author philip
+ */
+public class SessionBackedRequestFieldData extends RequestFieldData
+{
+  public SessionBackedRequestFieldData (@Nonnull final String sFieldName)
+  {
+    super (sFieldName);
+    _init ();
+  }
+
+  public SessionBackedRequestFieldData (@Nonnull final String sFieldName, @Nullable final String sDefaultValue)
+  {
+    super (sFieldName, sDefaultValue);
+    _init ();
+  }
+
+  public SessionBackedRequestFieldData (@Nonnull final String sFieldName, final int nDefaultValue)
+  {
+    super (sFieldName, nDefaultValue);
+    _init ();
+  }
+
+  @Nonnull
+  private String _getSessionFieldName ()
+  {
+    return "$$requestfield." + getFieldName ();
+  }
+
+  private void _init ()
+  {
+    final String sRequestValue = getRequestValueWithoutDefault ();
+    if (StringHelper.hasText (sRequestValue))
+      WebScopeManager.getSessionScope ().setAttribute (_getSessionFieldName (), sRequestValue);
+  }
+
+  @Override
+  public String getDefaultValue ()
+  {
+    final String sSuperDefaultValue = super.getDefaultValue ();
+    final IScope aSessionScope = WebScopeManager.getSessionScope (false);
+    return aSessionScope == null ? sSuperDefaultValue : aSessionScope.getAttributeAsString (_getSessionFieldName (),
+                                                                                            sSuperDefaultValue);
+  }
+
+  @Override
+  public boolean equals (final Object o)
+  {
+    return super.equals (o);
+  }
+
+  @Override
+  public int hashCode ()
+  {
+    return super.hashCode ();
+  }
+}
