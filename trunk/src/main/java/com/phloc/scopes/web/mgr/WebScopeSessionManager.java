d7c07c45c34eb96a181d46571c67f1dcda1124ea
ph@phloc.com <ph@phloc.com@1e497704-d800-dfe1-6598-1dc77ae1aeb0>
2012-07-24 10:39:11 +0000

    * Added methods in WebScopeSessionManager to handle ISessionWebScope objects only * Undeprecated WebScopeSessionManager

--- trunk/src/main/java/com/phloc/scopes/web/mgr/WebScopeSessionManager.java
+++ trunk/src/main/java/com/phloc/scopes/web/mgr/WebScopeSessionManager.java
@@ -17,20 +17,102 @@
  */
 package com.phloc.scopes.web.mgr;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.phloc.commons.annotations.ReturnsMutableCopy;
 import com.phloc.commons.annotations.UsedViaReflection;
+import com.phloc.scopes.nonweb.domain.ISessionScope;
 import com.phloc.scopes.nonweb.mgr.ScopeSessionManager;
+import com.phloc.scopes.web.domain.ISessionWebScope;
 
 /**
- * Present only for backwards code compatibility. {@link ScopeSessionManager} is
- * the class to be used.
+ * This is a specialization of {@link ScopeSessionManager} for web scopes.
  * 
  * @author philip
  */
-@Deprecated
 public final class WebScopeSessionManager extends ScopeSessionManager
 {
+  private static final Logger s_aLogger = LoggerFactory.getLogger (WebScopeSessionManager.class);
+
   @Deprecated
   @UsedViaReflection
   public WebScopeSessionManager ()
   {}
+
+  /**
+   * Get the web session scope with the specified ID. If no such scope exists,
+   * <code>null</code> is returned.
+   * 
+   * @param sScopeID
+   *        The ID to be resolved.
+   * @return <code>null</code> if no such scope exists or if the passed scope is
+   *         not a session web scopes.
+   */
+  @Nullable
+  public static ISessionWebScope getSessionWebScopeOfID (@Nullable final String sScopeID)
+  {
+    final ISessionScope aSessionScope = ScopeSessionManager.getInstance ().getSessionScopeOfID (sScopeID);
+    if (aSessionScope == null)
+      return null;
+    if (!(aSessionScope instanceof ISessionWebScope))
+    {
+      s_aLogger.info ("The passed scope ID '" + sScopeID + "' is not a session web scope!");
+      return null;
+    }
+    return (ISessionWebScope) aSessionScope;
+  }
+
+  /**
+   * Get the session web scope of the passed HTTP session.
+   * 
+   * @param aHttpSession
+   *        The HTTP session to get the scope from. May be <code>null</code>.
+   * @return <code>null</code> if either the HTTP session is <code>null</code>
+   *         or if no such scope exists.
+   */
+  @Nullable
+  public static ISessionWebScope getSessionWebScopeOfSession (@Nullable final HttpSession aHttpSession)
+  {
+    return aHttpSession == null ? null : getSessionWebScopeOfID (aHttpSession.getId ());
+  }
+
+  /**
+   * @return A non-<code>null</code>, mutable copy of all managed session web
+   *         scopes.
+   */
+  @Nonnull
+  @ReturnsMutableCopy
+  public static Collection <? extends ISessionWebScope> getAllSessionWebScopes ()
+  {
+    final List <ISessionWebScope> ret = new ArrayList <ISessionWebScope> ();
+    for (final ISessionScope aSessionScope : ScopeSessionManager.getInstance ().getAllSessionScopes ())
+      if (aSessionScope instanceof ISessionWebScope)
+        ret.add ((ISessionWebScope) aSessionScope);
+    return ret;
+  }
+
+  public static void destroyAllWebSessions ()
+  {
+    // destroy all session web scopes (make a copy, because we're invalidating
+    // the sessions!)
+    for (final ISessionWebScope aSessionScope : getAllSessionWebScopes ())
+    {
+      // Unfortunately we need a special handling here
+      if (aSessionScope.selfDestruct ().isContinue ())
+      {
+        // Remove from map
+        ScopeSessionManager.getInstance ().onScopeEnd (aSessionScope);
+      }
+      // Else the destruction was already started!
+    }
+  }
 }
