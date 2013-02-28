f8440d7aac5f7325b1a7037a3a9741e960b158e1
ph@phloc.com <ph@phloc.com@1e497704-d800-dfe1-6598-1dc77ae1aeb0>
2013-02-28 20:43:49 +0000

    [maven-release-plugin]  copy for tag phloc-scopes-6.0.0

---
+++ tags/phloc-scopes-6.0.0/src/main/java/com/phloc/scopes/singleton/SessionSingleton.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright (C) 2006-2013 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.scopes.singleton;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+import com.phloc.commons.annotations.MustImplementEqualsAndHashcode;
+import com.phloc.scopes.AbstractSingleton;
+import com.phloc.scopes.domain.ISessionScope;
+import com.phloc.scopes.mgr.ScopeManager;
+
+/**
+ * This is the base class for singleton objects that reside in the session
+ * non-web scope.
+ * 
+ * @see com.phloc.scopes.mgr.EScope#SESSION
+ * @author philip
+ */
+@MustImplementEqualsAndHashcode
+public abstract class SessionSingleton extends AbstractSingleton implements Serializable
+{
+  protected SessionSingleton ()
+  {
+    super ("getSessionSingleton");
+  }
+
+  /**
+   * @return The scope to be used for this type of singleton.
+   */
+  @Nonnull
+  private static ISessionScope _getStaticScope (final boolean bCreateIfNotExisting)
+  {
+    return ScopeManager.getSessionScope (bCreateIfNotExisting);
+  }
+
+  /**
+   * @return The scope to be used for this type of singleton.
+   */
+  @Override
+  @Nonnull
+  protected final ISessionScope getScope ()
+  {
+    return _getStaticScope (true);
+  }
+
+  /**
+   * Get the singleton object in the current session scope, using the passed
+   * class. If the singleton is not yet instantiated, a new instance is created.
+   * 
+   * @param aClass
+   *        The class to be used. May not be <code>null</code>. The class must
+   *        be public as needs to have a public no-argument constructor.
+   * @return The singleton object and never <code>null</code>.
+   */
+  @Nonnull
+  protected static final <T extends SessionSingleton> T getSessionSingleton (@Nonnull final Class <T> aClass)
+  {
+    return getSingleton (_getStaticScope (true), aClass);
+  }
+
+  /**
+   * Check if a singleton is already instantiated inside the current session
+   * scope
+   * 
+   * @param aClass
+   *        The class to be checked. May not be <code>null</code>.
+   * @return <code>true</code> if the singleton for the specified class is
+   *         already instantiated, <code>false</code> otherwise.
+   */
+  public static final boolean isSingletonInstantiated (@Nonnull final Class <? extends SessionSingleton> aClass)
+  {
+    return isSingletonInstantiated (_getStaticScope (false), aClass);
+  }
+
+  /**
+   * Get all singleton objects registered in the current session scope.
+   * 
+   * @return A non-<code>null</code> list with all instances of this class in
+   *         the current session scope.
+   */
+  @Nonnull
+  public static final List <SessionSingleton> getAllSingletons ()
+  {
+    return getAllSingletons (_getStaticScope (false), SessionSingleton.class);
+  }
+}
