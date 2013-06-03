d05c093be57d1276717175bd76addf5f2f67ead2
ph <ph@phloc.com>
2013-06-03 17:48:42 +0000

    Initial version

---
+++ phloc-event-scopes/src/test/java/com/phloc/event/scopes/ScopedEventManagerTest.java
@@ -0,0 +1,68 @@
+package com.phloc.event.scopes;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+
+import com.phloc.event.impl.BaseEvent;
+import com.phloc.scopes.mgr.EScope;
+import com.phloc.scopes.mock.ScopeTestRule;
+
+/**
+ * JUnit test for class {@link ScopedEventManager}.
+ * 
+ * @author philip
+ */
+public final class ScopedEventManagerTest
+{
+  @Rule
+  public final TestRule m_aScopeRule = new ScopeTestRule ();
+
+  @Test
+  public void testScoping ()
+  {
+    // prepare processing
+    final MockCountingObserver aGlobal = new MockCountingObserver ();
+    final MockCountingObserver aApp = new MockCountingObserver ();
+    final MockCountingObserver aRequest = new MockCountingObserver ();
+    assertTrue (ScopedEventManager.registerObserver (EScope.GLOBAL, aGlobal).isChanged ());
+    assertTrue (ScopedEventManager.registerObserver (EScope.APPLICATION, aApp).isChanged ());
+    assertTrue (ScopedEventManager.registerObserver (EScope.REQUEST, aRequest).isChanged ());
+    try
+    {
+      // check preconditions
+      assertEquals (0, aGlobal.getInvocationCount ());
+      assertEquals (0, aApp.getInvocationCount ());
+      assertEquals (0, aRequest.getInvocationCount ());
+
+      // process
+      assertTrue (ScopedEventManager.notifyObservers (new BaseEvent (MockCountingObserver.TOPIC)));
+
+      // check postconditions
+      assertEquals (1, aGlobal.getInvocationCount ());
+      assertEquals (1, aApp.getInvocationCount ());
+      assertEquals (1, aRequest.getInvocationCount ());
+    }
+    finally
+    {
+      // unregister
+      assertTrue (ScopedEventManager.unregisterObserver (EScope.GLOBAL, aGlobal).isChanged ());
+      assertTrue (ScopedEventManager.unregisterObserver (EScope.APPLICATION, aApp).isChanged ());
+      assertTrue (ScopedEventManager.unregisterObserver (EScope.REQUEST, aRequest).isChanged ());
+    }
+  }
+
+  @Test
+  public void testUnregister ()
+  {
+    final MockCountingObserver aGlobal = new MockCountingObserver ();
+    assertTrue (ScopedEventManager.registerObserver (EScope.GLOBAL, aGlobal).isChanged ());
+    assertTrue (ScopedEventManager.unregisterObserver (EScope.GLOBAL, aGlobal).isChanged ());
+
+    // unregister again should fail :)
+    assertTrue (ScopedEventManager.unregisterObserver (EScope.GLOBAL, aGlobal).isUnchanged ());
+  }
+}
