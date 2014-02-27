09063abd964c6387ba477363787776a1a3f8ae9a
ph@phloc.com <ph@phloc.com@1e497704-d800-dfe1-6598-1dc77ae1aeb0>
2014-02-27 16:34:16 +0000

    [maven-release-plugin]  copy for tag phloc-scopes-6.1.7

---
+++ tags/phloc-scopes-6.1.7/src/main/java/com/phloc/scopes/util/AbstractScopeAwareRunnable.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.scopes.util;
+
+import javax.annotation.Nonnull;
+
+import com.phloc.commons.annotations.Nonempty;
+import com.phloc.commons.callback.INonThrowingRunnable;
+import com.phloc.commons.string.StringHelper;
+import com.phloc.commons.string.ToStringGenerator;
+import com.phloc.scopes.mgr.ScopeManager;
+import com.phloc.scopes.mock.ScopeAwareTestSetup;
+
+/**
+ * Abstract implementation of {@link Runnable} that handles WebScopes correctly.
+ * 
+ * @author Philip Helger
+ */
+public abstract class AbstractScopeAwareRunnable implements INonThrowingRunnable
+{
+  private final String m_sApplicationID;
+  private final String m_sRequestID;
+  private final String m_sSessionID;
+
+  public AbstractScopeAwareRunnable ()
+  {
+    this (ScopeManager.getApplicationScope ().getID (),
+          ScopeAwareTestSetup.MOCK_REQUEST_SCOPE_ID,
+          ScopeAwareTestSetup.MOCK_SESSION_SCOPE_ID);
+  }
+
+  public AbstractScopeAwareRunnable (@Nonnull @Nonempty final String sApplicationID,
+                                     @Nonnull @Nonempty final String sRequestID,
+                                     @Nonnull @Nonempty final String sSessionID)
+  {
+    if (StringHelper.hasNoText (sApplicationID))
+      throw new IllegalArgumentException ("applicationID");
+    if (StringHelper.hasNoText (sRequestID))
+      throw new IllegalArgumentException ("requestID");
+    if (StringHelper.hasNoText (sSessionID))
+      throw new IllegalArgumentException ("sessionID");
+
+    m_sApplicationID = sApplicationID;
+    m_sRequestID = sRequestID;
+    m_sSessionID = sSessionID;
+  }
+
+  /**
+   * Implement your code in here.
+   */
+  protected abstract void scopedRun ();
+
+  public final void run ()
+  {
+    ScopeManager.onRequestBegin (m_sApplicationID, m_sRequestID, m_sSessionID);
+    try
+    {
+      scopedRun ();
+    }
+    finally
+    {
+      ScopeManager.onRequestEnd ();
+    }
+  }
+
+  @Override
+  public String toString ()
+  {
+    return new ToStringGenerator (this).append ("applicationID", m_sApplicationID)
+                                       .append ("requestID", m_sRequestID)
+                                       .append ("sessionID", m_sSessionID)
+                                       .toString ();
+  }
+}
